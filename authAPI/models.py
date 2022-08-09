76fc3e3e3b187cc8531f19e3ac90696c21bbd1c7
Ruhid Shukurlu <ruhids@maestro.st>
2022-08-10 02:00:37 +0400

    translation removed

--- authAPI/models.py
+++ authAPI/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from authAPI.managers import UserManager
-from django.utils.translation import gettext as _
 
 class User(AbstractUser):
     username = None
     email = models.EmailField("Email", unique=True)
-    profilePhoto = models.ImageField(_("Profile Photo"), upload_to='users/', blank=True, null=True)
+    profilePhoto = models.ImageField("Profile Photo", upload_to='users/', blank=True, null=True)
     # phone = models.CharField(_("Phone"), max_length=50)
 
     USERNAME_FIELD = 'email'
--- core/models.py
+++ core/models.py
@@ -1,24 +1,23 @@
 from django.db import models
-from django.utils.translation import gettext as _
 from cloudinary.models import CloudinaryField
 
 
 class Service(models.Model):
     
-    title = models.CharField(_("Title"), max_length=50)
-    description = models.TextField(_("Description"))
-    coverPhoto = models.ImageField(_("Cover Photo"), upload_to='services-rest/')
-    icon = models.ImageField(_("Icon"), upload_to='services-rest//')
-    averagePrice = models.IntegerField(_("Average Field"))
+    title = models.CharField("Title", max_length=50)
+    description = models.TextField("Description")
+    coverPhoto = models.ImageField("Cover Photo", upload_to='services-rest/')
+    icon = models.ImageField("Icon", upload_to='services-rest//')
+    averagePrice = models.IntegerField("Average Field")
 
     # moderation
-    isActive = models.BooleanField(_("Is Active"), default=True)
-    createdAt = models.DateTimeField(_("Created At"), auto_now_add=True)
-    updatedAt = models.DateTimeField(_("Updated At"), auto_now=True)
+    isActive = models.BooleanField("Is Active", default=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    updatedAt = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        verbose_name = _("Service")
-        verbose_name_plural = _("Services")
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
 
     def __str__(self):
         return self.title
@@ -30,22 +29,22 @@ SERVICE_TYPE_CHOICES = (
 )   
 class SubService(models.Model):
 
-    service = models.ForeignKey("core.Service", verbose_name=_("Service"), related_name='subServices', on_delete=models.CASCADE) 
-    title = models.CharField(_("Title"), max_length=50)
-    description = models.TextField(_("Description"))
-    coverPhoto = models.ImageField(_("Cover Photo"), upload_to='services-rest/')
-    icon = models.ImageField(_("Icon"), upload_to='services-rest/')
-    serviceCharge = models.IntegerField(_("Service Charge"), default=20)
-    serviceType = models.CharField(_("Service Type"), max_length=50, choices=SERVICE_TYPE_CHOICES)
+    service = models.ForeignKey("core.Service", verbose_name="Service", related_name='subServices', on_delete=models.CASCADE) 
+    title = models.CharField("Title", max_length=50)
+    description = models.TextField("Description")
+    coverPhoto = models.ImageField("Cover Photo", upload_to='services-rest/')
+    icon = models.ImageField("Icon", upload_to='services-rest/')
+    serviceCharge = models.IntegerField("Service Charge", default=20)
+    serviceType = models.CharField("Service Type", max_length=50, choices=SERVICE_TYPE_CHOICES)
 
     # moderation
-    isActive = models.BooleanField(_("Is Active"), default=True)
-    createdAt = models.DateTimeField(_("Created At"), auto_now_add=True)
-    updatedAt = models.DateTimeField(_("Updated At"), auto_now=True)
+    isActive = models.BooleanField("Is Active", default=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    updatedAt = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        verbose_name = _("SubService")
-        verbose_name_plural = _("SubServices")
+        verbose_name = "SubService"
+        verbose_name_plural = "SubServices"
 
     def __str__(self):
         return self.title
@@ -58,18 +57,18 @@ TYPE_CHOICES = (
 )
 class ServiceChoice(models.Model):
 
-    subService = models.ForeignKey("core.SubService", verbose_name=_("Sub Service"), related_name='serviceChoices', on_delete=models.CASCADE)
-    title = models.CharField(_("Title"), max_length=50)
-    type = models.CharField(_("Type"), choices=TYPE_CHOICES, default='radio', max_length=50)
+    subService = models.ForeignKey("core.SubService", verbose_name="Sub Service", related_name='serviceChoices', on_delete=models.CASCADE)
+    title = models.CharField("Title", max_length=50)
+    type = models.CharField("Type", choices=TYPE_CHOICES, default='radio', max_length=50)
 
     # moderation
-    isActive = models.BooleanField(_("Is Active"), default=True)
-    createdAt = models.DateTimeField(_("Created At"), auto_now_add=True)
-    updatedAt = models.DateTimeField(_("Updated At"), auto_now=True)
+    isActive = models.BooleanField("Is Active", default=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    updatedAt = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        verbose_name = _("ServiceChoice")
-        verbose_name_plural = _("ServiceChoices")
+        verbose_name = "ServiceChoice"
+        verbose_name_plural = "ServiceChoices"
 
     def __str__(self):
         return self.title
@@ -77,18 +76,18 @@ class ServiceChoice(models.Model):
 
 class ChoiceOption(models.Model):
 
-    choice = models.ForeignKey("core.ServiceChoice", verbose_name=_("Service Choice"), related_name='options', on_delete=models.CASCADE)
-    title = models.CharField(_("Title"), max_length=50)
-    taskerFiltering = models.BooleanField(_("Tasker Filtering"), default=False)
+    choice = models.ForeignKey("core.ServiceChoice", verbose_name="Service Choice", related_name='options', on_delete=models.CASCADE)
+    title = models.CharField("Title", max_length=50)
+    taskerFiltering = models.BooleanField("Tasker Filtering", default=False)
 
     # moderation
-    isActive = models.BooleanField(_("Is Active"), default=True)
-    createdAt = models.DateTimeField(_("Created At"), auto_now_add=True)
-    updatedAt = models.DateTimeField(_("Updated At"), auto_now=True)
+    isActive = models.BooleanField("Is Active", default=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    updatedAt = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        verbose_name = _("ChoiceOption")
-        verbose_name_plural = _("ChoiceOptions")
+        verbose_name = "ChoiceOption"
+        verbose_name_plural = "ChoiceOptions"
 
     def __str__(self):
         return self.title
@@ -97,30 +96,30 @@ class ChoiceOption(models.Model):
 
 class Tasker(models.Model):
 
-    user = models.OneToOneField("authAPI.User", verbose_name=_("User"), on_delete=models.CASCADE)
+    user = models.OneToOneField("authAPI.User", verbose_name="User", on_delete=models.CASCADE)
 
-    address = models.CharField(_("Address"), max_length=50)
-    coverPhoto = models.ImageField(_("Cover Photo"), upload_to='services-rest/', blank=True, null=True)
-    bio = models.TextField(_("Bio"))
-    rating = models.DecimalField(_("Rating"), max_digits=5, decimal_places=2, default=0)
-    completedTaskCount = models.IntegerField(_("Completed Tasks"), default=0)
+    address = models.CharField("Address", max_length=50)
+    coverPhoto = models.ImageField("Cover Photo", upload_to='services-rest/', blank=True, null=True)
+    bio = models.TextField("Bio")
+    rating = models.DecimalField("Rating", max_digits=5, decimal_places=2, default=0)
+    completedTaskCount = models.IntegerField("Completed Tasks", default=0)
 
-    workCities = models.ManyToManyField("core.City", verbose_name=_("Work Cities"), blank=True)
-    portofilos = models.ManyToManyField("core.Portfolio", verbose_name=_("Portofilos"), blank=True)
-    certificates = models.ManyToManyField("core.Certificate", verbose_name=_("Certificates"), blank=True)
+    workCities = models.ManyToManyField("core.City", verbose_name="Work Cities", blank=True)
+    portofilos = models.ManyToManyField("core.Portfolio", verbose_name="Portofilos", blank=True)
+    certificates = models.ManyToManyField("core.Certificate", verbose_name="Certificates", blank=True)
 
-    isAvailable = models.BooleanField(_("Is Available"), default=True)
-    topTasker = models.BooleanField(_("Top Tasker"), default=False)
-    supervisor = models.BooleanField(_("Supervisor"), default=False)
+    isAvailable = models.BooleanField("Is Available", default=True)
+    topTasker = models.BooleanField("Top Tasker", default=False)
+    supervisor = models.BooleanField("Supervisor", default=False)
 
     # moderation
-    isActive = models.BooleanField(_("Is Active"), default=True)
-    createdAt = models.DateTimeField(_("Created At"), auto_now_add=True)
-    updatedAt = models.DateTimeField(_("Updated At"), auto_now=True)
+    isActive = models.BooleanField("Is Active", default=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    updatedAt = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        verbose_name = _("Tasker")
-        verbose_name_plural = _("Taskers")
+        verbose_name = "Tasker"
+        verbose_name_plural = "Taskers"
 
     def __str__(self):
         return self.user.first_name
@@ -128,15 +127,15 @@ class Tasker(models.Model):
 
 class TaskerSkill(models.Model):
 
-    tasker = models.ForeignKey("core.Tasker", verbose_name=_("Tasker"), related_name='skills', on_delete=models.CASCADE)
-    subService = models.ForeignKey("core.SubService", verbose_name=_("Sub Service"), related_name='skills', on_delete=models.CASCADE)
-    option = models.ForeignKey("core.ChoiceOption", related_name='skills', verbose_name=_("Option"), on_delete=models.CASCADE)
-    priceType = models.CharField(_("Price Type"), max_length=50, choices=SERVICE_TYPE_CHOICES, blank=True)
-    price = models.IntegerField(_("Price"))
+    tasker = models.ForeignKey("core.Tasker", verbose_name="Tasker", related_name='skills', on_delete=models.CASCADE)
+    subService = models.ForeignKey("core.SubService", verbose_name="Sub Service", related_name='skills', on_delete=models.CASCADE)
+    option = models.ForeignKey("core.ChoiceOption", related_name='skills', verbose_name="Option", on_delete=models.CASCADE)
+    priceType = models.CharField("Price Type", max_length=50, choices=SERVICE_TYPE_CHOICES, blank=True)
+    price = models.IntegerField("Price")
 
     class Meta:
-        verbose_name = _("TaskerSkill")
-        verbose_name_plural = _("TaskerSkills")
+        verbose_name = "TaskerSkill"
+        verbose_name_plural = "TaskerSkills"
 
     def __str__(self):
         return self.option.title
@@ -157,29 +156,29 @@ STATUS_CHOICES = (
 
 class Order(models.Model):
 
-    customer = models.ForeignKey("authAPI.User", verbose_name=_("Customer"), related_name='orders', on_delete=models.CASCADE)
-    tasker = models.ForeignKey("core.Tasker", verbose_name=_("Tasker"), related_name='orders', on_delete=models.CASCADE)
-    subService = models.ForeignKey("core.SubService", verbose_name=_("SubService"), related_name='orders', on_delete=models.CASCADE)
-    options = models.ManyToManyField("core.ChoiceOption", verbose_name=_("Options"))
+    customer = models.ForeignKey("authAPI.User", verbose_name="Customer", related_name='orders', on_delete=models.CASCADE)
+    tasker = models.ForeignKey("core.Tasker", verbose_name="Tasker", related_name='orders', on_delete=models.CASCADE)
+    subService = models.ForeignKey("core.SubService", verbose_name="SubService", related_name='orders', on_delete=models.CASCADE)
+    options = models.ManyToManyField("core.ChoiceOption", verbose_name="Options")
 
-    startDate = models.DateTimeField(_("Start Date"), auto_now=False, auto_now_add=False)
-    address = models.CharField(_("Address"), max_length=50)
-    status = models.CharField(_("Stauts"), choices=STATUS_CHOICES, default='new', max_length=50)
-    reference = models.CharField(_("Reference"), max_length=50)
-    detail = models.TextField(_("Detail"))
-    photos = models.ManyToManyField("core.OrderPhoto", verbose_name=_("Photos"), blank=True)
+    startDate = models.DateTimeField("Start Date", auto_now=False, auto_now_add=False)
+    address = models.CharField("Address", max_length=50)
+    status = models.CharField("Stauts", choices=STATUS_CHOICES, default='new', max_length=50)
+    reference = models.CharField("Reference", max_length=50)
+    detail = models.TextField("Detail")
+    photos = models.ManyToManyField("core.OrderPhoto", verbose_name="Photos", blank=True)
 
-    totalAmount = models.DecimalField(_("Total Amount"), max_digits=5, decimal_places=2, default=0)
-    discount = models.DecimalField(_("Discount"), max_digits=5, decimal_places=2, default=0)
+    totalAmount = models.DecimalField("Total Amount", max_digits=5, decimal_places=2, default=0)
+    discount = models.DecimalField("Discount", max_digits=5, decimal_places=2, default=0)
 
     # moderation
-    isActive = models.BooleanField(_("Is Active"), default=True)
-    createdAt = models.DateTimeField(_("Created At"), auto_now_add=True)
-    updatedAt = models.DateTimeField(_("Updated At"), auto_now=True)
+    isActive = models.BooleanField("Is Active", default=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    updatedAt = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        verbose_name = _("Order")
-        verbose_name_plural = _("Orders")
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
 
     def __str__(self):
         return self.subService.title
@@ -188,23 +187,23 @@ class Order(models.Model):
 
 class OrderPhoto(models.Model):
 
-    photo = models.ImageField(_("Photo"), upload_to='services-rest/')
+    photo = models.ImageField("Photo", upload_to='services-rest/')
 
     class Meta:
-        verbose_name = _("OrderPhoto")
-        verbose_name_plural = _("OrderPhotos")
+        verbose_name = "OrderPhoto"
+        verbose_name_plural = "OrderPhotos"
     
 
 
 
 class Portfolio(models.Model):
 
-    title = models.CharField(_("Title"), max_length=50)
-    photo = models.ImageField(_("Photo"), upload_to='services-rest/')
+    title = models.CharField("Title", max_length=50)
+    photo = models.ImageField("Photo", upload_to='services-rest/')
 
     class Meta:
-        verbose_name = _("Portfolio")
-        verbose_name_plural = _("Portfolios")
+        verbose_name = "Portfolio"
+        verbose_name_plural = "Portfolios"
 
     def __str__(self):
         return self.title
@@ -212,12 +211,12 @@ class Portfolio(models.Model):
 
 class Certificate(models.Model):
 
-    title = models.CharField(_("Title"), max_length=50)
-    photo = models.ImageField(_("Photo"), upload_to='services-rest/')
+    title = models.CharField("Title", max_length=50)
+    photo = models.ImageField("Photo", upload_to='services-rest/')
 
     class Meta:
-        verbose_name = _("Certificate")
-        verbose_name_plural = _("Certificates")
+        verbose_name = "Certificate"
+        verbose_name_plural = "Certificates"
 
     def __str__(self):
         return self.title
@@ -225,12 +224,12 @@ class Certificate(models.Model):
 
 class City(models.Model):
 
-    name = models.CharField(_("Name"), max_length=50)
-    isActive = models.BooleanField(_("Is Active"), default=True)
+    name = models.CharField("Name", max_length=50)
+    isActive = models.BooleanField("Is Active", default=True)
 
     class Meta:
-        verbose_name = _("City")
-        verbose_name_plural = _("Cities")
+        verbose_name = "City"
+        verbose_name_plural = "Cities"
 
     def __str__(self):
         return self.name
@@ -239,20 +238,20 @@ class City(models.Model):
 
 class Comment(models.Model):
 
-    author = models.ForeignKey("authAPI.User", verbose_name=_("Author"), on_delete=models.CASCADE)
-    subService = models.ForeignKey("core.SubService", verbose_name=_("Sub Service"), on_delete=models.SET_NULL, blank=True, null=True)
-    tasker = models.ForeignKey("core.Tasker", verbose_name=_("Tasker"), on_delete=models.SET_NULL, blank=True, null=True)
-    text = models.TextField(_("Text"))
-    rating = models.IntegerField(_("Rating"))
+    author = models.ForeignKey("authAPI.User", verbose_name="Author", on_delete=models.CASCADE)
+    subService = models.ForeignKey("core.SubService", verbose_name="Sub Service", on_delete=models.SET_NULL, blank=True, null=True)
+    tasker = models.ForeignKey("core.Tasker", verbose_name="Tasker", on_delete=models.SET_NULL, blank=True, null=True)
+    text = models.TextField("Text")
+    rating = models.IntegerField("Rating")
 
     # moderation
-    isActive = models.BooleanField(_("Is Active"), default=True)
-    createdAt = models.DateTimeField(_("Created At"), auto_now_add=True)
-    updatedAt = models.DateTimeField(_("Updated At"), auto_now=True)
+    isActive = models.BooleanField("Is Active", default=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    updatedAt = models.DateTimeField("Updated At", auto_now=True)
 
     class Meta:
-        verbose_name = _("Comment")
-        verbose_name_plural = _("Comments")
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
 
     def __str__(self):
         return self.author.first_name
